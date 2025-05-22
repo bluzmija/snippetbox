e0e6faad8089d3e87a97a18ff1096463d1fb4c4a
bluzmija <172497559+bluzmija@users.noreply.github.com>
2025-05-22 17:00:46 +0200

    Built homepage in html and added templating

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,13 +2,36 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	w.Write([]byte("Hello from Snippetbox!"))
+
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
+
+	//koristi template.ParseFiles metodu da procitas template file u template set
+	//ako dode do errora, logamo detailed error message i nakon toga se vrati iz handlera
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	//koristimo ts.Execute() da ispisemo template set ko content responsa
+	//drugi parametar ts.Execute je dinamicki, pa ga za sad ostavljamo praznog
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+	}
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
